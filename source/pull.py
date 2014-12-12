6e16dec0b9b209885314990cddf254e0689308e4
Mindaugas Barysas <mindaugas.barysas@ongr.io>
2014-12-12 11:20:42 +0200

    Documentation upgrades, quite final for now.

--- source/pull.py
+++ source/pull.py
@@ -1,7 +1,5 @@
 # -- autopull repos
 
-
-
 lines = [line.strip() for line in open('%s/sources/.repos' % (os.path.dirname(os.path.realpath(__file__))))]
 
 for line in lines:
@@ -9,6 +7,4 @@ for line in lines:
         os.system("cd sources; mkdir %s; cd %s; git init; git remote add -f origin %s; git config core.sparsecheckout true; echo 'Resources/doc' > .git/info/sparse-checkout; echo 'Resources/API' >> .git/info/sparse-checkout; git pull origin master;" % (line.split("/")[-1], line.split("/")[-1], line))
 
 # fix links to githubs' .rst files.
-os.system("grep -rl '.rst' sources/ | grep -v '/.git/' | xargs perl -pe 's/(\.\. _.*: |\`.*<)(.*)\.rst(>\`_|)/$1:doc:`$2`/g' -i")
-os.system("grep -rl ':doc:' sources/ | grep -v '/.git/' | xargs perl -pe 's/(.*)(`.*):doc:/$1:doc:/g' -i")
-os.system("grep -rl ':doc:' sources/ | grep -v '/.git/' | xargs perl -pe 's/(\.\. _.*:):doc:(.*)/$1$2.html/g' -i")
+os.system("grep -rl '.rst' sources/ | grep -v '/.git/' | xargs perl -pe 's/\`(.*)<(.*)\.rst>\`_/$1:doc:`$2`/g' -i")
