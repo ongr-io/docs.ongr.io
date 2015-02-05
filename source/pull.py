e3006f30b1879f14b8933ae452a06cfb0a4d5c4e
Mindaugas Barysas <mindaugas.barysas@nfq.lt>
2015-02-05 13:48:14 +0200

    Update pull.py

--- source/pull.py
+++ source/pull.py
@@ -11,7 +11,7 @@ for line in lines:
             doc_directory = 'Resources/doc'
 
         repository_name = repository_url.split("/")[-1]
-        os.system("mkdir _components; cd _components; mkdir %s; cd %s; git init; git remote add -f origin %s; git config core.sparsecheckout true; echo '%s' > .git/info/sparse-checkout; echo 'Resources/API' >> .git/info/sparse-checkout; git reset --hard; git pull origin master; cd ../../components; rm %s; ln -s ../_components/%s/%s %s; cd .." % (repository_name, repository_name, repository_url, doc_directory, repository_name, repository_name, doc_directory, repository_name))
+        os.system("mkdir _components; cd _components; mkdir %s; cd %s; git init; git remote add -f origin %s; git config core.sparsecheckout true; echo '%s' > .git/info/sparse-checkout; echo 'Resources/API' >> .git/info/sparse-checkout; git reset --hard; git pull origin master; cd ../../components; rm -rf %s; ln -s ../_components/%s/%s %s; cd .." % (repository_name, repository_name, repository_url, doc_directory, repository_name, repository_name, doc_directory, repository_name))
 
 # fix links to githubs' .rst files.
 os.system("grep -rl '.rst' components/ | grep -v '/.git/' | xargs perl -pe 's/\`(.*?)<(.*?)\.rst>\`_/:doc:`$1<$2>`/g' -i")
