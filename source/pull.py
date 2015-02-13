5ab6eeb67e5d61e51fb075d7e355652848d47e6c
Aivaras Gotovskis <aivaras.gotovskis@ongr.io>
2015-02-13 15:05:42 +0200

    Use modified rtd theme with correct links to repos.

--- source/pull.py
+++ source/pull.py
@@ -12,6 +12,7 @@ for line in lines:
 
         repository_name = repository_url.split("/")[-1]
         os.system("mkdir _components; cd _components; mkdir %s; cd %s; git init; git remote add -f origin %s; git config core.sparsecheckout true; echo '%s' > .git/info/sparse-checkout; echo 'Resources/API' >> .git/info/sparse-checkout; git reset --hard; git pull origin master; cd ../../components; rm -rf %s; ln -s ../_components/%s/%s %s; cd .." % (repository_name, repository_name, repository_url, doc_directory, repository_name, repository_name, doc_directory, repository_name))
+        os.system("mkdir _themes; cd _themes; mkdir sphinx_rtd_theme; cd sphinx_rtd_theme; mkdir doc_paths; cd doc_paths; echo '%s' > %s.html" % (doc_directory, repository_name))
 
 # Fix links to githubs' .rst files.
 os.system("grep -rl '.rst' _components/ | grep -v '/.git/' | xargs perl -pe 's/\`(.*?)<(.*?)\.rst>\`_/:doc:`$1<$2>`/g' -i")
