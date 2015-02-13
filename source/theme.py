4f354f6137294743daf4b5f93b1ccbc8ea6b0354
Aivaras Gotovskis <aivaras.gotovskis@ongr.io>
2015-02-13 17:10:44 +0200

    Changed to ongr-io repository.

--- source/theme.py
+++ source/theme.py
@@ -3,4 +3,4 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [ "_themes" ]
 html_style = 'my'
 
-os.system("mkdir _themes; cd _themes; git init; git remote add -f origin https://github.com/GrandLTU/sphinx_rtd_theme.git; git config core.sparsecheckout true; echo 'sphinx_rtd_theme' > .git/info/sparse-checkout; git reset --hard; git pull origin ongr;")
+os.system("mkdir _themes; cd _themes; git init; git remote add -f origin https://github.com/ongr-io/sphinx_rtd_theme.git; git config core.sparsecheckout true; echo 'sphinx_rtd_theme' > .git/info/sparse-checkout; git reset --hard; git pull origin ongr;")
